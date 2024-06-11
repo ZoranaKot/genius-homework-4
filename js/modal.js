4f580d2502096fdc06263444046303033fed0162
ZoranaKot <165703549+ZoranaKot@users.noreply.github.com>
2024-06-11 23:43:53 +0300

    Delete js directory

--- js/modal.js
+++
@@ -1,8 +0,0 @@
-const modal = document.querySelector(".backdrop");
-const modalBtnOpen = document.querySelector(".modalBtnOpen");
-const modalBtnClose = document.querySelector(".modalBtnClose");
-
-const toggleModal = () => modal.classList.toggle("is-hidden");
-
-modalBtnOpen.addEventListener("click", toggleModal);
-modalBtnClose.addEventListener("click", toggleModal);
